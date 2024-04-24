e03f774f0893c7162cd3a485dbadd86267060ddc
Yurii Shkoropad <lvivgeorge@gmail.com>
2024-04-24 10:53:42 +0300

    scroll to top on navigation

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '@core/components/header';
 import './styles/main.scss';
 import { Footer } from '@core/components/footer';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { firebaseService } from '@core/services/firebase.service';
 
 const App = () => {
+  const { pathname } = useLocation();
+  const mainRef = useRef<HTMLElement>(null);
+
   useEffect(() => {
     firebaseService.init();
   }, []);
 
+  useEffect(() => {
+    mainRef.current.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     // h-1 for min-h
-    <main className="bg-background min-h-full h-full overflow-auto">
+    <main
+      className="bg-background min-h-full h-full overflow-auto"
+      ref={mainRef}
+    >
       <div className="p-5 min-h-[calc(100%-64px)]">
         <Header />
         <Outlet />
--- src/app/core/components/post-layout/post-layout-content.tsx
+++ src/app/core/components/post-layout/post-layout-content.tsx
@@ -24,7 +24,7 @@ const PostLayoutContent = ({ article }: PostLayoutContentProps) => {
       components={{ a: LinkRenderer }}
       className="post-layout"
       remarkPlugins={[remarkMath, remarkAlert]}
-      rehypePlugins={[rehypeKatex]}
+      rehypePlugins={[[rehypeKatex, { strict: false }]]}
     >
       {article}
     </Markdown>
