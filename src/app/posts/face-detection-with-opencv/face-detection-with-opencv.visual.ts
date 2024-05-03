ff4cf88f2a8b116a80e6be8a5b3cc5f3f8d0f88a
Yurii Shkoropad <lvivgeorge@gmail.com>
2024-05-03 10:59:14 +0300

    add border effect

--- src/app/posts/face-detection-with-opencv/face-detection-with-opencv.visual.ts
+++ src/app/posts/face-detection-with-opencv/face-detection-with-opencv.visual.ts
@@ -12,6 +12,8 @@ class FaceDetectionWithOpenCVVisual {
   private height: number;
 
   private capture: cv.VideoCapture;
+  private frameIndex: number = 0;
+  private videoTrack: MediaStreamTrack;
 
   constructor(canvasEl: HTMLCanvasElement, mediaStream: MediaStream) {
     this.canvasEl = canvasEl;
@@ -24,28 +26,43 @@ class FaceDetectionWithOpenCVVisual {
     if (this.frameId) {
       cancelAnimationFrame(this.frameId);
     }
-    this.videoEl.pause();
-    this.videoEl.remove();
+    if (this.videoEl) {
+      this.videoEl.pause();
+      this.videoEl.remove();
+    }
     this.mediaStream.getTracks().forEach((track) => {
       track.stop();
     });
   }
 
   private async init(): Promise<void> {
+    this.videoTrack = this.mediaStream.getVideoTracks()[0];
     this.setDimensions();
+
     this.videoEl = document.createElement('video');
     this.videoEl.height = this.height;
     this.videoEl.width = this.width;
     await playVideoMediaStream(this.videoEl, this.mediaStream);
+    this.applyConstraints();
 
     this.capture = new cv.VideoCapture(this.videoEl);
     this.renderVideo();
   }
 
+  private applyConstraints(): void {
+    const constraints = {
+      width: { min: 640, ideal: 1280 },
+      height: { min: 480, ideal: 720 },
+      advanced: [{ width: 1920, height: 1280 }, { aspectRatio: 1.333 }]
+    };
+
+    if (this.mediaStream.active) {
+      this.videoTrack.applyConstraints(constraints);
+    }
+  }
+
   private setDimensions(): void {
-    const settings: MediaTrackSettings = this.mediaStream
-      .getVideoTracks()[0]
-      .getSettings();
+    const settings: MediaTrackSettings = this.videoTrack.getSettings();
     this.width = settings.width;
     this.height = settings.height;
 
@@ -57,18 +74,35 @@ class FaceDetectionWithOpenCVVisual {
     this.frameId = requestAnimationFrame(async () => {
       await this.updateFrame();
       this.renderVideo();
+      this.frameIndex++;
     });
   }
 
   private async updateFrame(): Promise<void> {
     try {
       const sourceMat = new cv.Mat(this.height, this.width, cv.CV_8UC4);
-      const dstMat = new cv.Mat(this.height, this.width, cv.CV_8UC4);
+      const grayMat = new cv.Mat(this.height, this.width, cv.CV_8UC4);
+      const borderMat = new cv.Mat(this.height, this.width, cv.CV_8UC4);
+
+      const s = new cv.Scalar(255, 0, 0, 255);
+
       this.capture.read(sourceMat);
-      cv.cvtColor(sourceMat, dstMat, cv.COLOR_RGBA2GRAY);
-      cv.imshow(this.canvasEl, dstMat);
+      cv.cvtColor(sourceMat, grayMat, cv.COLOR_RGBA2GRAY);
+      cv.copyMakeBorder(
+        grayMat,
+        borderMat,
+        this.height / 2,
+        this.height / 2,
+        this.width / 2,
+        this.width / 2,
+        cv.BORDER_DEFAULT,
+        s
+      );
+      cv.imshow(this.canvasEl, borderMat);
+
       sourceMat.delete();
-      dstMat.delete();
+      grayMat.delete();
+      borderMat.delete();
     } catch (e) {
       this.stop();
       console.error('error', e);
